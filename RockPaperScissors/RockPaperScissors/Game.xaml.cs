b4abeb560ae529ca56542c449ed78491ae1f5714
TomasOstlind <Tomaseriksson993@hotmail.com>
2015-11-26 11:17:40 +0100

    1.0

--- RockPaperScissors/RockPaperScissors/Game.xaml.cs
+++ RockPaperScissors/RockPaperScissors/Game.xaml.cs
@@ -18,13 +18,48 @@ namespace RockPaperScissors
 {
     public sealed partial class Game : Page
     {
+        //------------Varibles-------------------
+        private int vs = 0; //To check if user have choosen PvP or PvC
+
         public Game()
         {
             this.InitializeComponent();
+            InitializeGui();
         }
+        /// <summary>
+        /// Handles the pre-GUI
+        /// </summary>
+        private void InitializeGui()
+        {
+            EnterNameP2.Visibility = Visibility.Collapsed;
+            GridButtonsP1.Visibility = Visibility.Collapsed;
+            GridButtonsP2.Visibility = Visibility.Collapsed;
+
+        }
+
         private void SplitView(object sender, RoutedEventArgs e)
         {
             MyPane.SplitView.IsPaneOpen = !MyPane.SplitView.IsPaneOpen;
+        }
+        /// <summary>
+        /// Switch between PvP and PvC
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChoosePMode_Click(object sender, RoutedEventArgs e)
+        {
+            if (vs == 0)
+            {
+                vs = 1;
+                EnterNameP2.Visibility = Visibility.Visible;
+                btnChoosePMode.Content = "PvC";
+            }
+            else 
+            {
+                vs = 0;
+                EnterNameP2.Visibility = Visibility.Collapsed;
+                btnChoosePMode.Content = "PvP";
+            }
         }
     }
 }
